54aebee3ce9d63b89f2c9f746c29401533a5cf5e
Ricardo Falcão <ricardomanuelfalcao20@gmail.com>
2021-03-13 11:48:14 +0000

    MES: Added Conveyor and Region classes

---
+++ mes/src/main/java/ii/pfc/conveyor/Conveyor.java
@@ -0,0 +1,46 @@
+package ii.pfc.conveyor;
+
+import ii.pfc.part.Part;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Conveyor {
+
+    private final int id;
+
+    private final EnumConveyorType type;
+
+    private final List<Part> partList;
+
+    public Conveyor(int id, EnumConveyorType type) {
+        this.id = id;
+        this.type = type;
+
+        this.partList = new ArrayList<>();
+    }
+
+    /*
+
+     */
+
+    public int getId() {
+        return id;
+    }
+
+    public EnumConveyorType getType() {
+        return type;
+    }
+
+    public List<Part> getParts() {
+        return Collections.unmodifiableList(partList);
+    }
+
+    /*
+
+     */
+
+    public boolean hasPart(Part part) {
+        return partList.contains(part);
+    }
+}
