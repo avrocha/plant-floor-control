36fc82b9a9df91a6a51ee913fb0f48d44c6ebc75
Daniel <daniel.barros_99@hotmail.com>
2021-05-10 23:05:55 +0100

    feature_db_conn: GUI tables data dynamics changes

--- mes/src/main/java/ii/pfc/gui/GUI.java
+++ mes/src/main/java/ii/pfc/gui/GUI.java
@@ -10,7 +10,10 @@ import ii.pfc.part.Part;
 import ii.pfc.part.PartType;
 
 import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.time.Duration;
 import java.util.Collection;
 import java.util.EventObject;
 import java.util.HashMap;
@@ -38,9 +41,9 @@ public class GUI extends JFrame {
         JLabel tab2Title = new JLabel("Unloaded Parts");
         JLabel tab3Title = new JLabel("Inventory");
 
-        JPanel p1 = new JPanel();
-        JPanel p2 = new JPanel();
-        JPanel p3 = new JPanel();
+        JPanel p1=new JPanel();
+        JPanel p2=new JPanel();
+        JPanel p3=new JPanel();
 
         p1.setLayout(new BoxLayout(p1, BoxLayout.PAGE_AXIS));
         p2.setLayout(new BoxLayout(p2, BoxLayout.PAGE_AXIS));
@@ -53,19 +56,33 @@ public class GUI extends JFrame {
             Info table about Assemblers
          */
 
+        // Assembler   | Operation time | Number of operated parts
+        // Assembler 1 |        0       |        0
+        // ...
+
+
+
+        Collection<Conveyor> assemblerConveyors = routingManager.getConveyors(EnumConveyorType.ASSEMBLY);
+
         String assemblers[] = {"Assembler", "Operation time", "Number of operated parts"};
 
-        Object[][] dataAssemblers = {
-                {"Assembler 1", 0, 0},
-                {"Assembler 2", 0, 0},
-                {"Assembler 3", 0, 0},
-                {"Assembler 4", 0, 0},
-                {"Assembler 5", 0, 0},
-                {"Assembler 6", 0, 0},
-                {"Assembler 7", 0, 0},
-                {"Assembler 8", 0, 0}};
+        Object[][] dataAssemblers = new Object[assemblerConveyors.size()][assemblers.length];
 
-        JTable assemblersTable = new JTable(dataAssemblers, assemblers) {
+        int assembleIndex = 0;
+        for(assembleIndex = 0; assembleIndex < assemblers.length;assembleIndex ++) {
+            dataAssemblers[assembleIndex][0] = String.format("Assembler %d", assembleIndex + 1);
+        }
+
+        int assemblerConveyorIndex = 0;
+        for(Conveyor assemblerConveyor : assemblerConveyors) {
+            Map<PartType, Duration> processDurations = databaseManager.fetchProcessDurations(assemblerConveyor.getId());
+            //int value = (Duration);
+            //dataAssemblers[assemblerConveyorIndex][1 + assemblerConveyorIndex] = value;
+
+
+        }
+
+        JTable assemblersTable= new JTable(dataAssemblers, assemblers){
             @Override
             public boolean editCellAt(int row, int column, EventObject event) {
                 return false;
@@ -77,15 +94,15 @@ public class GUI extends JFrame {
            Info table about Unloading Parts
          */
 
-
         // Type | Slider 1 | Slider 2 | ... | Total
         // P1   |        0 |        1 | ... | 1
+        // ...
 
         Collection<Conveyor> sliderConveyors = routingManager.getConveyors(EnumConveyorType.SLIDER);
         String[] unloadInfo = new String[1 + sliderConveyors.size() + 1];
         unloadInfo[0] = "Type";
 
-        for (int i = 0; i < sliderConveyors.size(); i++) {
+        for(int i = 0; i < sliderConveyors.size(); i++) {
             unloadInfo[1 + i] = String.format("Slider %d", i + 1);
         }
 
@@ -94,7 +111,7 @@ public class GUI extends JFrame {
         Object[][] dataUnloadedParts = new Object[partTypes.size()][1 + sliderConveyors.size() + 1];
 
         int typeIndex = 0;
-        for (PartType type : partTypes) {
+        for(PartType type : partTypes) {
             dataUnloadedParts[typeIndex][0] = type.getName();
             dataUnloadedParts[typeIndex][unloadInfo.length - 1] = 0;
 
@@ -102,7 +119,7 @@ public class GUI extends JFrame {
         }
 
         int sliderConveyorIndex = 0;
-        for (Conveyor sliderConveyor : sliderConveyors) {
+        for(Conveyor sliderConveyor : sliderConveyors) {
             Map<PartType, Integer> unloadedParts = databaseManager.countUnloadedParts(sliderConveyor.getId());
 
             typeIndex = 0;
@@ -116,7 +133,7 @@ public class GUI extends JFrame {
             sliderConveyorIndex++;
         }
 
-        JTable unloadedPartsTable = new JTable(dataUnloadedParts, unloadInfo) {
+        JTable unloadedPartsTable = new JTable(dataUnloadedParts, unloadInfo){
             @Override
             public boolean editCellAt(int row, int column, EventObject event) {
                 return false;
@@ -128,17 +145,21 @@ public class GUI extends JFrame {
             Info table about Inventory
          */
 
+        // Type | Stored | Processing | ... | Completed
+        // P1   |      0 |        1   | ... | 0
+        //  ...
+
         String[] partHeaders = new String[1 + partStates.length];
         partHeaders[0] = "Type";
 
-        for (int i = 0; i < partStates.length; i++) {
+        for(int i = 0; i < partStates.length; i++) {
             partHeaders[i + 1] = partStates[i].getDisplayName();
         }
 
         Collection<Part> parts = databaseManager.fetchParts();
         Map<Part.PartState, Multimap<PartType, Part>> stateMap = new HashMap<>();
 
-        for (Part part : parts) {
+        for(Part part : parts) {
             Part.PartState state = part.getState();
             Multimap<PartType, Part> typeMap = stateMap.get(state);
 
@@ -153,10 +174,10 @@ public class GUI extends JFrame {
         Object[][] dataParts = new Object[partTypes.size()][1 + partStates.length];
 
         int i = 0;
-        for (PartType type : partTypes) {
+        for(PartType type : partTypes) {
             dataParts[i][0] = type.getName();
 
-            for (int j = 0; j < partStates.length; j++) {
+            for(int j = 0; j < partStates.length; j++) {
                 if (stateMap.containsKey(partStates[j])) {
                     dataParts[i][j + 1] = stateMap.get(partStates[j]).get(type).size();
                 } else {
@@ -167,7 +188,7 @@ public class GUI extends JFrame {
             i++;
         }
 
-        JTable inventoryTable = new JTable(dataParts, partHeaders) {
+        JTable inventoryTable= new JTable(dataParts, partHeaders){
             @Override
             public boolean editCellAt(int row, int column, EventObject event) {
                 return false;
@@ -175,35 +196,35 @@ public class GUI extends JFrame {
         };
         inventoryTable.setRowSelectionAllowed(false);
 
-        JTabbedPane tp = new JTabbedPane();
+        JTabbedPane tp=new JTabbedPane();
 
-        tp.add("Assemblers", p1);
-        tp.add("Unloaded parts", p2);
+        tp.add("Assemblers",p1);
+        tp.add("Unloaded parts",p2);
         tp.add("Inventory", p3);
 
         p1.add(tab1Title);
-        p1.add(Box.createRigidArea(new Dimension(2, 10)));
-        p1.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        p1.add(Box.createRigidArea(new Dimension(2,10)));
+        p1.setBorder(BorderFactory.createEmptyBorder(15,15,15,15));
 
         p1.add(assemblersTable.getTableHeader());
         p1.add(assemblersTable);
 
         p2.add(tab2Title);
-        p2.add(Box.createRigidArea(new Dimension(2, 10)));
-        p2.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        p2.add(Box.createRigidArea(new Dimension(2,10)));
+        p2.setBorder(BorderFactory.createEmptyBorder(15,15,15,15));
 
         p2.add(unloadedPartsTable.getTableHeader());
         p2.add(unloadedPartsTable);
 
         p3.add(tab3Title);
-        p3.add(Box.createRigidArea(new Dimension(2, 10)));
-        p3.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        p3.add(Box.createRigidArea(new Dimension(2,10)));
+        p3.setBorder(BorderFactory.createEmptyBorder(15,15,15,15));
 
         p3.add(inventoryTable.getTableHeader());
         p3.add(inventoryTable);
 
         this.add(tp);
-        this.setSize(width, height);
+        this.setSize(width,height);
     }
 
 }
